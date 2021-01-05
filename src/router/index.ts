672af17c2ceb5122dec4551006cf95b417741e8f
eggplant60 <nagoyan240@gmail.com>
2021-01-05 13:37:23 +0900

    6章終了時

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Home from "../views/Home.vue";
+import editComponent from '@/views/Edit.vue';
 
 Vue.use(VueRouter);
 
@@ -18,7 +19,12 @@ const routes: Array<RouteConfig> = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+  },
+  {
+    path: '/edit',
+    name: 'Edit',
+    component: editComponent,
+  },
 ];
 
 const router = new VueRouter({
